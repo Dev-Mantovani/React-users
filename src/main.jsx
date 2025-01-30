b39246cd16c69038e64f04dd4ea5d649676f2923
Dev-Mantovani <dev.mantovani@hotmail.com>
2025-01-29 21:06:08 -0300

    React User Att

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // default
+import { GlobalStyles } from './styles/GlobalStyles.js'
+/*import {App, App2} from './App.jsx' // sem default */
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <GlobalStyles />
     <App />
   </StrictMode>,
 )
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background-color: black;
+
+    h1 {
+        color: purple;
+
+
+    }
+
+`
+export const Title = styled.h2`
+color: blue;
+font-size: 30px;
+
+
+
+`
