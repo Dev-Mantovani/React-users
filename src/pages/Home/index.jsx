9b004a9cb0a68e81714125fd420738147d44762b
Dev-Mantovani <dev.mantovani@hotmail.com>
2025-02-11 19:47:15 -0300

    Introduzindo Back End

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,20 @@
+import { useRef } from 'react'
 
 import { Title, TopBackground, Container, Form, ContainerInputs, Input, Button, InputLabel } from './styles'
 import UsersImage from '../../assets/users.png'
 
 function Home() {
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser() {
+    console.log ( inputName.current.value)
+
+
+
+  }
 
   return (
     <>
@@ -19,24 +31,24 @@ function Home() {
               <div>
                 <InputLabel> Nome <span>*</span>
                 </InputLabel>
-                <Input type='text' placeholder='Nome do usuário'></Input>
+                <Input type='text' placeholder='Nome do usuário' ref={inputName}></Input>
               </div>
 
               <div>
                 <InputLabel> Idade <span>*</span>
                 </InputLabel>
-                <Input type='number' placeholder='Idade do usuário'></Input>
+                <Input type='number' placeholder='Idade do usuário' ref={inputAge}></Input>
               </div>
           </ContainerInputs>
               <div style={{width: '100%'}}>
                 <InputLabel> E-mail <span>*</span>
                 </InputLabel>
-                <Input type='email' placeholder='E-mail do usuário'></Input>
+                <Input type='email' placeholder='E-mail do usuário' ref={inputEmail}></Input>
               </div>
             
           
 
-          <Button>Cadastrar Usuario</Button>
+          <Button type='button' onClick={registerNewUser}>Cadastrar Usuario</Button>
         </Form>
       </Container>
 
